a16b87c726cdab4a1bb75dc42b147acc344ce6b0
Allison Howard <alliemhow@gmail.com>
2019-09-15 08:26:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const baseURL = 'https://api.github.com';
-const user = '<YOUR_USERNAME>';
+const user = 'cheshicat';
+const token = '';
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+  return token;
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
+  fetch(
+    `${baseURL}/repos/${repo}/forks`, {
+      method: "POST",
+      headers: {
+        Authorization: `token ${getToken()}`
+      }
+    })
+    .then(res => res.json())
+    .then(json => showResults(json));
 }
 
 function showResults(json) {
